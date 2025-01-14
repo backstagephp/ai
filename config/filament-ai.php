38dc310a1bce86ef9cd2f7b283f02bff44db3fb2
Mark van Eijk <mark@vaneijk.co>
2025-01-14 12:18:19 +0100

    wip

--- config/filament-ai.php
+++ config/filament-ai.php
@@ -1,11 +1,22 @@
 <?php
 
+use EchoLabs\Prism\Enums\Provider;
+
 return [
+    'providers' => [
+        'gpt-4o-mini' => Provider::OpenAI,
+    ],
+
     'action' => [
         'label' => 'AI',
         'icon' => 'heroicon-o-sparkles',
         'modal' => [
             'heading' => 'Generate with AI',
         ],
+    ],
+
+    'configuration' => [
+        'max_tokens' => 100,
+        'temperature' => 0.7,
     ],
 ];
--- src/FilamentAI.php
+++ src/FilamentAI.php
@@ -6,10 +6,14 @@ use Filament\Forms\Set;
 use EchoLabs\Prism\Prism;
 use EchoLabs\Prism\Enums\Provider;
 use Filament\Forms\Components\Field;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Notifications\Notification;
 use EchoLabs\Prism\Exceptions\PrismException;
 use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Get;
 
 class FilamentAI
 {
@@ -24,15 +28,53 @@ class FilamentAI
                         ->modalHeading(config('filament-ai.action.modal.heading'))
                         ->modalSubmitActionLabel('Generate')
                         ->form([
+                            Select::make('model')
+                                ->label('Model')
+                                ->options(
+                                    collect(config('filament-ai.providers'))
+                                        ->mapWithKeys(fn($provider, $model) => [
+                                            $model => $model . ' (' . $provider->name . ')',
+                                        ]),
+                                )
+                                ->default(key(config('filament-ai.providers'))),
+
                             Textarea::make('prompt')
                                 ->label('Prompt')
                                 ->autosize()
                                 ->default($prompt),
+
+                            Section::make('configuration')
+                                ->heading('Configuration')
+                                ->schema([
+                                    TextInput::make('temperature')
+                                        ->numeric()
+                                        ->label('Temperature')
+                                        ->default(config('filament-ai.configuration.temperature'))
+                                        ->helperText('The higher the temperature, the more creative the text')
+                                        ->maxValue(1)
+                                        ->minValue(0)
+                                        ->step('0.1'),
+                                    TextInput::make('max_tokens')
+                                        ->numeric()
+                                        ->label('Max tokens')
+                                        ->default(config('filament-ai.configuration.max_tokens'))
+                                        ->helperText('The maximum number of tokens to generate')
+                                        ->step('10')
+                                        ->minValue(0)
+                                        ->suffixAction(
+                                            Action::make('increase')
+                                                ->icon('heroicon-o-plus')
+                                                ->action(fn(Set $set, Get $get) => $set('max_tokens', $get('max_tokens') + 100)),
+                                        ),
+                                ])
+                                ->columns(2)
+                                ->collapsed()
+                                ->collapsible()
                         ])
                         ->action(function ($data) use ($component, $set) {
                             try {
                                 $response = Prism::text()
-                                    ->using(Provider::OpenAI, 'gpt-4o-mini')
+                                    ->using(config('filament-ai.providers.' . $data['model']), $data['model'])
                                     ->withPrompt($data['prompt'])
                                     ->generate();
 
